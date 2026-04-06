a9ab3cb6b75c9777644e49f966501094846d9bfe
doddipallishreehari397-hash <243263127+doddipallishreehari397-hash@users.noreply.github.com>
2026-04-06 18:14:06 +0530

    Add files via upload

---
+++ data.py
@@ -0,0 +1,48 @@
+timetable = {
+    "Monday": {
+        "9:00-10:00": "Mathematics",
+        "10:00-11:00": "Physics",
+        "11:15-12:15": "Artificial Intelligence",
+        "1:00-2:00": "Data Structures",
+        "2:00-3:00": "Computer Science"
+    },
+    "Tuesday": {
+        "9:00-10:00": "Data Structures",
+        "10:00-11:00": "Computer Science",
+        "11:15-12:15": "Mathematics",
+        "1:00-2:00": "Physics",
+        "2:00-3:00": "Artificial Intelligence"
+    },
+    "Wednesday": {
+        "9:00-10:00": "Physics",
+        "10:00-11:00": "Artificial Intelligence",
+        "11:15-12:15": "Data Structures",
+        "1:00-2:00": "Mathematics",
+        "2:00-3:00": "Computer Science"
+    },
+    "Thursday": {
+        "9:00-10:00": "Computer Science",
+        "10:00-11:00": "Mathematics",
+        "11:15-12:15": "Artificial Intelligence",
+        "1:00-2:00": "Physics",
+        "2:00-3:00": "Data Structures"
+    },
+    "Friday": {
+        "9:00-10:00": "Data Structures",
+        "10:00-11:00": "Physics",
+        "11:15-12:15": "Computer Science",
+        "1:00-2:00": "Mathematics",
+        "2:00-3:00": "Artificial Intelligence"
+    }
+}
+
+exams = {
+    "mid_exam": {
+        "start_date": "2026-04-20",
+        "end_date": "2026-04-25"
+    },
+    "final_exam": {
+        "start_date": "2026-06-10",
+        "end_date": "2026-06-18"
+    }
+}
